12e2dac463bca5087a04cb27f121c22cf403b52e
Recogabbooo <137157203+Recogabbooo@users.noreply.github.com>
2023-09-12 13:34:27 +0200

    first commit

---
+++ custom.js
@@ -0,0 +1,21 @@
+const contests = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
+
+listItems.forEach((item, idx) => { 
+    item.addEventListener('click', () => {
+
+        hideAllContents()
+        hideAllItems()
+
+        item.classList.add('active')
+        contests[idx].classList.add('show')
+    } )
+})
+
+function hideAllContents(){ 
+    contests.forEach(content => content.classList.remove('show'))
+}
+
+function hideAllItems(){
+    listItems.forEach(item => item.classList.remove('active'))
+}
